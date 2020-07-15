32ec83423cc4cc348629a39badfd53a27585d0e5
Kayla Kuhlman <kakoreo4@gmail.com>
2020-07-15 18:31:18 -0500

    removed build from gitignore

---
+++ client/build/precache-manifest.5289efb188791217d8b9dbc05114ff2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146a49dc4c51c0699430eb37a71c9eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d46fd42a7a747020b3e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e1b362586a5db084378b",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "8d46fd42a7a747020b3e",
+    "url": "/static/js/2.7858b4d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7858b4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b362586a5db084378b",
+    "url": "/static/js/main.17c1b7e1.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
