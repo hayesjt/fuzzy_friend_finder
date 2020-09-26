7d897146cdc5e29abb8081c5cecd7b97938a83a3
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-09-26 13:06:37 -0500

    fixed home events for mobile function and bugs in home values section

--- client/src/components/home/home_components/Values.js
+++ client/src/components/home/home_components/Values.js
@@ -12,41 +12,41 @@ export const Values = () => (
 
         <h1 class="section_title">OUR MISSION</h1>
 
-<CardDeck>
-                <Card>
-                    <Card.Img variant="top" src={Values1} />
-                    <Card.Body>
-                        <Card.Title>Supporting Both Ends of the Leash</Card.Title>
-                        <Card.Text>
-                            We strive to support both animals and owners through various services and intense application matching. We ensure each adoption is a perfect pair so each animal can have a forever home. 
+        <CardDeck>
+            <Card>
+                <Card.Img variant="top" src={Values1} />
+                <Card.Body>
+                    <Card.Title>Supporting Both Ends of the Leash</Card.Title>
+                    <Card.Text>
+                        We strive to support both animals and owners through various services and intense application matching. We ensure each adoption is a perfect pair so each animal can have a forever home.
     </Card.Text>
-                        <Button variant="primary">Adoptable Animals</Button>
-                    </Card.Body>
-                </Card>
-   
-                <Card>
-                    <Card.Img variant="top" src={Values2} />
-                    <Card.Body>
-                        <Card.Title>Quality Support and Care</Card.Title>
-                        <Card.Text>
+                    <Button variant="primary">Adoptable Animals</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Img variant="top" src={Values2} />
+                <Card.Body>
+                    <Card.Title>Quality Support and Care</Card.Title>
+                    <Card.Text>
                         We run and operate our own vet clinic onsite to ensure ever single animal we take in gets quality care before going to its forever home. Once adopted, all animals are welcome to use our vet clinic at a discounted rate.
     </Card.Text>
-                        <Button variant="primary">Veterinary Services</Button>
-                    </Card.Body>
-                </Card>
-          
-                <Card>
-                    <Card.Img variant="top" src={Values3} />
-                    <Card.Body>
-                        <Card.Title>Awareness and Community </Card.Title>
-                        <Card.Text>
-                        Every month we host a variety of volunteer and public events. Through these events we hope to increase the awareness of homeless pets and adoption options. We invite everyone to stop in to one of our events. 
+                    <Button variant="primary">Veterinary Services</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Img variant="top" src={Values3} />
+                <Card.Body>
+                    <Card.Title>Awareness and Community </Card.Title>
+                    <Card.Text>
+                        Every month we host a variety of volunteer and public events. Through these events we hope to increase the awareness of homeless pets and adoption options. We invite everyone to stop in to one of our events.
     </Card.Text>
-                        <Button variant="primary">Upcoming Events</Button>
-                    </Card.Body>
-                </Card>
-                </CardDeck>
-    
+                    <Button variant="primary">Upcoming Events</Button>
+                </Card.Body>
+            </Card>
+        </CardDeck>
+
     </React.Fragment>
 
 )
